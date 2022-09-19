dd7a36cdb2e2c34524f193d17278cab90334396f
dmytro-bieliei <dmbieliei@gmail.com>
2022-09-19 22:06:33 +0300

    add task solution

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,18 @@
 // Создать рекурсивную функцию вычисления факториала числа.Факториал - произведение всех натуральных чисел от 1 до x.
 // 5! = 1 * 2 * 3 * 4 * 5 = 120
 
+function factorial(number){
+  if(number === 0) {
+    return 1;
+  }else if (number < 0){
+    return 'enter correct number';
+  }
+  // if (number > 1) {
+  //   return number * factorial(number - 1);
+  // } else {
+  //   return 1;
+  // }
+  return number > 1
+    ? number * factorial(number - 1)
+    : 1  
+};
